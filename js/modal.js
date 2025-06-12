669e4e899e9c9b2611c04999e2c7048199ac93de
AndriiKhrushch <140267164+AndriiKhrushch@users.noreply.github.com>
2025-06-12 21:15:45 +0300

    adaptive updated

--- js/modal.js
+++ js/modal.js
@@ -1,8 +1,8 @@
-const mobileMenu = document.querySelector(".backdrop");
-const menuBtnOpen = document.querySelector(".modal-btn-open");
-const menuBtnClose = document.querySelector(".modal-btn-close");
+const modal = document.querySelector(".backdrop");
+const openBtns = document.querySelectorAll(".modal__btn-open");
+const closeBtns = document.querySelectorAll(".modal-btn-close");
 
 const toggleModal = () => modal.classList.toggle("is__hidden");
 
-menuBtnOpen.addEventListener("click", toggleModal);
-menuBtnClose.addEventListener("click", toggleModal);
+openBtns.forEach((btn) => btn.addEventListener("click", toggleModal));
+closeBtns.forEach((btn) => btn.addEventListener("click", toggleModal));
